f81df88b4afd9e9043d9982c25a6a4f62b0d0c90
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-04-25 16:23:59 +0700

    fixing dashboard user

--- api/dashboard/dashboard_user.php
+++ api/dashboard/dashboard_user.php
@@ -1,14 +1,6 @@
 <?php
-ini_set('session.cookie_samesite', 'None');
-ini_set('session.cookie_secure', '1');
-ini_set('session.cookie_httponly', '1');
-session_start();
-
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Sat, 01 Jan 2000 00:00:00 GMT");
-
-if (!isset($_SESSION['username'])) {
+$username = $_GET['user'] ?? '';
+if(empty($username)){
     header("Location: /index.html");
     exit;
 }
