a2f10d2576f423ec0aba94b045acc8910233ac69
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-04-25 14:36:09 +0700

    update koneksi

--- api/koneksi.php
+++ api/koneksi.php
@@ -6,12 +6,12 @@ $dbname = 'suara_wisata';
 $port   = 4000;
 
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4;ssl-mode=VERIFY_IDENTITY";
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
     $connection = new PDO($dsn, $user, $pass, [
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
-        PDO::MYSQL_ATTR_SSL_CA               => '/etc/ssl/certs/ca-certificates.crt',
-        PDO::ATTR_ERRMODE                    => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT                    => 8,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::MYSQL_ATTR_SSL_CA                => '/etc/ssl/certs/ca-certificates.crt',
+        PDO::ATTR_ERRMODE                     => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT                     => 8,
     ]);
 } catch (PDOException $e) {
     http_response_code(500);
