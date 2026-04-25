79a075b4bb9cc761bf2d5d2caecd9d0cdc9b6511
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-04-25 13:50:56 +0700

    ubah test

--- api/test.php
+++ api/test.php
@@ -1,4 +1,4 @@
 <?php
-echo "PHP JALANN!";
-phpinfo();
-?>
+echo "PHP version: " . phpversion() . "<br>";
+echo "mysqli loaded: " . (extension_loaded('mysqli') ? 'YES' : 'NO') . "<br>";
+echo "pdo_mysql loaded: " . (extension_loaded('pdo_mysql') ? 'YES' : 'NO') . "<br>";
