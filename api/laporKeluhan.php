f1a3b1aee4ae7edbaa9497ad6bff250b234b7587
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-04-25 18:12:48 +0700

    update laporan keluhan wisata

--- api/laporKeluhan.php
+++ api/laporKeluhan.php
@@ -18,12 +18,16 @@ $base_url = "/suaraWisata/";
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laporan Keluhan Wisata</title>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="<?= $base_url ?>plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> -->
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -162,12 +166,19 @@ $base_url = "/suaraWisata/";
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $base_url ?>plugins/jquery/jquery.min.js"></script>
+<script src="<?= $base_url ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?= $base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $base_url ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="<?= $base_url ?>dist/js/adminlte.js"></script>
+<script src="<?= $base_url ?>js/scriptLaporan.js"></script>
+<script src="<?= $base_url ?>js/logout.js"></script>
+<!-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <script src="dist/js/adminlte.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="js/scriptLaporan.js"></script>
-<script src="js/logout.js"></script>
+<script src="js/logout.js"></script> -->
 
 <script>
   history.pushState(null, null, window.location.href);
