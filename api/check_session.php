8bf13496e6e9ec90d010e004aad02f79b7855203
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-04-25 16:13:07 +0700

    update check session

--- api/check_session.php
+++ api/check_session.php
@@ -1,15 +1,9 @@
 <?php
+ini_set('session.cookie_samesite', 'None');
+ini_set('session.cookie_secure', '1');
+ini_set('session.cookie_httponly', '1');
 session_start();
 
-//Membuat response file JSON
-header("Content-Type: application/json");
-//memastikan bahwa tidak menytimpan cache response
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-
-//mengecek apakah ada data usernamenya atau tidak,lalu kirim dalam format JSON true/false
-//kalau true--> tetap di dashboard,kalau false redirect ke index.php
-echo json_encode([
-    'loggedIn' => isset($_SESSION['username'])
-]);
+header('Content-Type: application/json');
+echo json_encode(['loggedIn' => isset($_SESSION['username'])]);
 ?>
