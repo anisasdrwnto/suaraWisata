04abbb4e9cf12a342f7a692fb995af8ddbbd2b52
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-04-27 23:01:17 +0700

    fixing get laporan

--- api/get_laporan.php
+++ api/get_laporan.php
@@ -1,11 +1,21 @@
 <?php
+header('Content-Type: application/json');
+
 require __DIR__ . '/koneksi.php';
+
+if (!$conn) {
+    die(json_encode(["error" => "Koneksi database gagal"]));
+}
 
 $query = "SELECT nama_pelapor, isi_laporan 
           FROM laporan_wisata
           ORDER BY id_laporan ASC";
 
 $result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die(json_encode(["error" => mysqli_error($conn)]));
+}
 
 $data = [];
 
